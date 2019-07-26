543838404655791de85ec7793e14df07daada8eb
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2019-07-26 23:01:15 +0530

    Complete Events and MiscEvent ViewModels

---
+++ app/src/main/java/com/dvm/appd/bosm/dbg/events/view/MiscEventsFragment.kt
@@ -0,0 +1,31 @@
+package com.dvm.appd.bosm.dbg.events.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.dvm.appd.bosm.dbg.R
+import com.dvm.appd.bosm.dbg.events.viewmodel.MiscEventsViewModel
+import com.dvm.appd.bosm.dbg.events.viewmodel.MiscEventsViewModelFactory
+
+class MiscEventsFragment : Fragment() {
+
+    private lateinit var miscEventsViewViewModel: MiscEventsViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        miscEventsViewViewModel = ViewModelProviders.of(this, MiscEventsViewModelFactory())[MiscEventsViewModel::class.java]
+
+        val view = inflater.inflate(R.layout.fra_misc_events, container, false)
+
+        miscEventsViewViewModel.miscEvents.observe(this, Observer {
+
+            Log.d("MiscEventsFrag", "Observed")
+        })
+        return view
+    }
+}
