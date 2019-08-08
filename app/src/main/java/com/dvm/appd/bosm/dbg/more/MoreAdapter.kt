1a325003b1144f8299162e12b350981955fcc804
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-08-08 15:38:26 +0530

    Add adapter for more layout

--- app/src/main/java/com/dvm/appd/bosm/dbg/more/MoreAdapter.kt
+++ app/src/main/java/com/dvm/appd/bosm/dbg/more/MoreAdapter.kt
@@ -1,2 +1,39 @@
 package com.dvm.appd.bosm.dbg.more
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dvm.appd.bosm.dbg.R
+import kotlinx.android.synthetic.main.card_recycler_more.view.*
+
+class MoreAdapter(private val listener: onMoreItemClicked) : RecyclerView.Adapter<MoreAdapter.moreViewHolder>() {
+
+    var moreItems : List<String> = emptyList()
+
+    interface onMoreItemClicked {
+        fun moreButtonClicked(item: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): moreViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_recycler_more,parent,false)
+        return moreViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return moreItems.size
+    }
+
+    override fun onBindViewHolder(holder: moreViewHolder, position: Int) {
+        holder.title.text = moreItems[position]
+        holder.parent.setOnClickListener {
+            listener.moreButtonClicked(position)
+        }
+    }
+
+    inner class moreViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        var title = view.text_card_more_title
+        var thimbnail = view.img_card_more_thumbnail
+        var parent = view.linear_card_more_parent
+    }
+}
