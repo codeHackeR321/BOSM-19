bad595b8765ac4009be4595844fda6518bad05ef
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-08-08 15:14:49 +0530

    Add basic code for more fragment

---
+++ app/src/main/java/com/dvm/appd/bosm/dbg/more/MoreFragment.kt
@@ -0,0 +1,26 @@
+package com.dvm.appd.bosm.dbg.more
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.dvm.appd.bosm.dbg.R
+
+class MoreFragment : Fragment() {
+
+    var moreItems = listOf("Contact Us", "Developers", "Map", "EPC Blog", "HPC Blog", "Picture Gallery")
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_more, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
