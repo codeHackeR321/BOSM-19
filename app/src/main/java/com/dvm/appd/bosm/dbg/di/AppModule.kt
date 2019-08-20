a9677e18a0713234dacbbf03a717014efbbeb727
ishita Aggarwal <e.agrawal03@gmail.com>
2019-08-21 01:28:04 +0530

    Minor changes

--- app/src/main/java/com/dvm/appd/bosm/dbg/di/AppModule.kt
+++ app/src/main/java/com/dvm/appd/bosm/dbg/di/AppModule.kt
@@ -52,7 +52,6 @@ class AppModule(private val application: Application) {
     @Singleton
     fun providesAppDatabase(application: Application): AppDatabase {
         return Room.databaseBuilder(application, AppDatabase::class.java, "bosm.db")
-            .fallbackToDestructiveMigration()
             .build()
     }
 
