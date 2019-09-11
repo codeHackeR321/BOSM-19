fd658514f3cbb107dec050741fdfb72785462eab
ishita Aggarwal <e.agrawal03@gmail.com>
2019-09-11 18:07:22 +0530

    Added visual polish to onboarding screens

--- app/src/main/java/com/dvm/appd/bosm/dbg/wallet/views/adapters/StallsAdapter.kt
+++ app/src/main/java/com/dvm/appd/bosm/dbg/wallet/views/adapters/StallsAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.dvm.appd.bosm.dbg.R
 import com.dvm.appd.bosm.dbg.wallet.data.room.dataclasses.StallData
 import kotlinx.android.synthetic.main.adapter_wallet_stalls.view.*
@@ -25,6 +26,7 @@ class StallsAdapter (private val listener:OnStallSelectedListener): RecyclerView
 
     override fun onBindViewHolder(holder: StallsViewHolder, position: Int) {
         holder.stallName.text = stalls[position].stallName
+        Glide.with(holder.itemView.context!!).load("").placeholder(R.drawable.ic_fast_food).into(holder.stallImg)
         holder.stallImg.setOnClickListener {
             listener.stallSelected(stalls[position])
         }
