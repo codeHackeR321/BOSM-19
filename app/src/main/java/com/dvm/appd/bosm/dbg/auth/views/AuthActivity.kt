e524535b24f16116b9b98bdd06f6a451f8dc13cf
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-09-12 16:43:51 +0530

    Add color to status bar in auth activity

--- app/src/main/java/com/dvm/appd/bosm/dbg/auth/views/AuthActivity.kt
+++ app/src/main/java/com/dvm/appd/bosm/dbg/auth/views/AuthActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 
 import androidx.lifecycle.ViewModelProviders
@@ -66,6 +68,14 @@ class AuthActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.status_bar_auth))
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
